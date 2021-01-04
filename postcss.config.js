c49830534b258c392b0e3f0031249687ba8e2ed3
ArcticJunkie <pikka99@gmail.com>
2021-01-04 17:48:28 +0530

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+
+
+  ]
+
+}
