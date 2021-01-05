93c8cd567fb60356f021460acc844fd04923df00
ArcticJunkie <pikka99@gmail.com>
2021-01-05 18:17:30 +0530

    added grid to results, scaled app to screen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { useState } from 'react';
 let unicodes = [
   { name: 'A', unicode: ['à', 'â', '@', 'á', 'æ', 'å'] },
   { name: 'E', unicode: ['ė', 'ę', 'é', 'è', 'ë'] },
-  { name: 'U', unicode: ['û', 'ü', 'ù', 'ú', 'ū'] }
+  { name: 'I', unicode: ['î', 'ï', 'í', 'ī', 'į', 'ì'] },
+  { name: 'O', unicode: ['ô', 'ö', 'ò', 'ó', 'œ', 'ø', 'ō', 'õ'] },
+  { name: 'U', unicode: ['û', 'ü', 'ù', 'ú', 'ū'] },
+  { name: 'TM', unicode: ['™'] }
 ]
 
 
@@ -13,13 +16,14 @@ function App() {
   return (
     <div className="App flex flex-col items-baseline space-y-6 p-20 font-mono">
       <span className="">search for special character by basic character.</span>
-      <span className="">This is a test deployment, enter vowels only.</span>
+      <span className="">This is a test deployment, try entering a vowel.</span>
       <input
+        className="bg-gray-200 rounded-sm p-2 shadow-sm"
         type="text"
         placeholder="search..."
         onChange={(event) => { setSearchTerm(event.target.value); }} />
 
-      {unicodes.filter((val) => {
+      <div className="grid gap-4 grid-cols-3">{unicodes.filter((val) => {
         if (searchTerm === "") {
           return ""
         }
@@ -31,6 +35,7 @@ function App() {
           return <div className=""><button className="flex flex-col space-y-4 items-center w-36 py-6 bg-gray-200 shadow-sm rounded-md focus:bg-green-200 focus:outline-none focus:ring-2 focus:ring-green-600" onClick={(event) => { navigator.clipboard.writeText(val) }}><p>{val}</p><span className="text-xs text-gray-700">Copy to clipboard</span></button></div>
         })
       })}
+      </div>
 
     </div>
   );
