3c47dc70c69a285ffaf219168d8a92757509238c
ArcticJunkie <pikka99@gmail.com>
2021-01-05 15:27:51 +0530

    button focus styling, added tooltips and quick instructions at top

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 
 let unicodes = [
   { name: 'A', unicode: ['à', 'â', '@', 'á', 'æ', 'å'] },
-  { name: 'E', unicode: ['ė', 'ę', 'é', 'è', 'ë'] }
+  { name: 'E', unicode: ['ė', 'ę', 'é', 'è', 'ë'] },
+  { name: 'U', unicode: ['û', 'ü', 'ù', 'ú', 'ū'] }
 ]
 
 
@@ -11,13 +12,15 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
   return (
     <div className="App flex flex-col items-baseline space-y-6 p-20 font-mono">
-      <span className="">search for special character by basic character, click to copy</span>
+      <span className="">search for special character by basic character.</span>
+      <span className="">This is a test deployment, enter vowels only.</span>
       <input
         type="text"
         placeholder="search..."
         onChange={(event) => { setSearchTerm(event.target.value); }} />
+
       {unicodes.filter((val) => {
-        if (searchTerm == "") {
+        if (searchTerm === "") {
           return ""
         }
         else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
@@ -25,9 +28,10 @@ function App() {
         }
       }).map((val, key) => {
         return val.unicode.map((val, key) => {
-          return <div><button className="w-36 py-6 bg-blue-50 rounded-md" onClick={(event) => { navigator.clipboard.writeText(val) }}>{val}</button></div>
+          return <div className=""><button className="flex flex-col space-y-4 items-center w-36 py-6 bg-gray-200 shadow-sm rounded-md focus:bg-green-200 focus:outline-none focus:ring-2 focus:ring-green-600" onClick={(event) => { navigator.clipboard.writeText(val) }}><p>{val}</p><span className="text-xs text-gray-700">Copy to clipboard</span></button></div>
         })
       })}
+
     </div>
   );
 }
