a9c22bd681841ba4716b76720823ec849597c18e
ArcticJunkie <pikka99@gmail.com>
2021-01-05 14:21:55 +0530

    copy to clipboard added, basic styling on copy button added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 
 let unicodes = [
-  { name: 'A', unicode: ['à', 'Â', '@'] },
-  { name: 'E', unicode: ['ė', 'ę'] }
+  { name: 'A', unicode: ['à', 'â', '@', 'á', 'æ', 'å'] },
+  { name: 'E', unicode: ['ė', 'ę', 'é', 'è', 'ë'] }
 ]
 
 
@@ -10,20 +10,23 @@ function App() {
 
   const [searchTerm, setSearchTerm] = useState('');
   return (
-    <div className="App flex flex-col items-baseline p-20">
+    <div className="App flex flex-col items-baseline space-y-6 p-20 font-mono">
+      <span className="">search for special character by basic character, click to copy</span>
       <input
         type="text"
         placeholder="search..."
         onChange={(event) => { setSearchTerm(event.target.value); }} />
       {unicodes.filter((val) => {
-        if (setSearchTerm == "") {
-          return val
+        if (searchTerm == "") {
+          return ""
         }
         else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
           return val
         }
       }).map((val, key) => {
-        return <div className="py-4 text-2xl font-bold text-gray-700">{val.unicode}</div>
+        return val.unicode.map((val, key) => {
+          return <div><button className="w-36 py-6 bg-blue-50 rounded-md" onClick={(event) => { navigator.clipboard.writeText(val) }}>{val}</button></div>
+        })
       })}
     </div>
   );
